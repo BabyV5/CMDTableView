ec054ada800dc1a3475577139144048b4c627133
mac <mac@MyMacBook.local>
2015-05-13 20:20:45 +0800

    Initial Commit

---
+++ CMDTableView/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  CMDTableView
+//
+//  Created by mac on 15/5/13.
+//  Copyright (c) 2015年 Baby_V5. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
